c6a0d40d59727ba1211797a1691284de3170abe3
JuanAntonio-Uni <jluis.torres@hotmail.com>
2026-05-30 10:50:31 -0600

    Se agregaron los nombres de los fantasmas, se epecifico cuantos puntos dan los dos tipos de bolitas y se coloco la indicacion de presionar enter para iniciar el juego

--- main.cpp
+++ main.cpp
@@ -10,6 +10,10 @@
 //#define COL 40
 #define ANCHO_PTLL 600
 #define LARGO_PTLL 800
+#define CL_FROJO GetColor(0xFF0000FF)
+#define CL_FROSA GetColor(0xffb8ffFF)
+#define CL_FAZUL GetColor(0x00ffffFF)
+#define CL_FNARANJA GetColor(0xffb852FF)
 
 //void menu();
 //void gamebody();
@@ -22,7 +26,19 @@ int main(){
         //void menu();
         //Draw
         BeginDrawing();
-        DrawText(Punt_txt, 175, 0, 40, WHITE);
+        DrawText(Punt_txt, 204, 0, 30, WHITE);
+        DrawText("CHARACTER / NICKNAME", 150, 70, 30, WHITE);
+        DrawText("-SHADOW     \"BLINKY\"", 155, 120, 30, CL_FROJO);
+        DrawText("-SPEEDY     \"PINKY\"", 155, 200, 30, CL_FROSA);
+        DrawText("-BASHFUL    \"INKY\"", 155, 280, 30, CL_FAZUL);
+        DrawText("-POKEY       \"CLYDE\"", 155, 360, 30, CL_FNARANJA);
+        DrawCircle(250, 500, 3, WHITE);
+        DrawText("10", 270, 485, 30, WHITE);
+        DrawText("PTS", 315, 490, 20, WHITE);
+        DrawCircle(250, 560, 10, WHITE);
+        DrawText("50", 270, 545, 30, WHITE);
+        DrawText("PTS", 315, 550, 20, WHITE);
+        DrawText("PRESS ENTER TO START", 100, 660, 30, CL_FNARANJA);
         EndDrawing();
     }
     CloseWindow();
