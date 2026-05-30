32047fcf3c586fe609cdf4e1549fd43134b51215
JuanAntonio-Uni <jluis.torres@hotmail.com>
2026-05-30 07:31:18 -0600

    Se crea la ventana para el juego con un aspecto 3:4 (600 x 800), ademas mostras "HIGH SCORE", iniciando el menu

--- main.cpp
+++ main.cpp
@@ -6,12 +6,25 @@
 #include <stdlib.h>
 #include <raylib.h>
 
-#define REN 40
-#define COL 40
+//#define REN 40
+//#define COL 40
+#define ANCHO_PTLL 600
+#define LARGO_PTLL 800
 
-void menu();
-void gamebody();
+//void menu();
+//void gamebody();
 
 int main(){
-    void menu();//xddd
+    char Punt_txt[] = "HIGH SCORE";
+    InitWindow(ANCHO_PTLL, LARGO_PTLL, "Pac-Man-Classic-Game");
+    SetTargetFPS(60);
+    while(WindowShouldClose() == false){
+        //void menu();
+        //Draw
+        BeginDrawing();
+        DrawText(Punt_txt, 175, 0, 40, WHITE);
+        EndDrawing();
+    }
+    CloseWindow();
+    return 0;
 }
