5beb7e49335854fac8fe0b2e2cb5ed745889dd52
JuanAntonio-Uni <jluis.torres@hotmail.com>
2026-05-30 15:20:28 -0600

    Se empeso el espacio para el juego y se puso una linea de prueba

--- main.cpp
+++ main.cpp
@@ -15,32 +15,51 @@
 #define CL_FAZUL GetColor(0x00ffffFF)
 #define CL_FNARANJA GetColor(0xffb852FF)
 
+void menu();
+
 //void menu();
 //void gamebody();
 
 int main(){
-    char Punt_txt[] = "HIGH SCORE";
     InitWindow(ANCHO_PTLL, LARGO_PTLL, "Pac-Man-Classic-Game");
     SetTargetFPS(60);
+    bool menuactivo = true;
     while(WindowShouldClose() == false){
         //void menu();
+        //Event handling
+        if(menuactivo){
+            if(IsKeyPressed(KEY_ENTER)){
+                menuactivo = false;
+            }
+        }
         //Draw
         BeginDrawing();
-        DrawText(Punt_txt, 204, 0, 30, WHITE);
-        DrawText("CHARACTER / NICKNAME", 150, 70, 30, WHITE);
-        DrawText("-SHADOW     \"BLINKY\"", 155, 120, 30, CL_FROJO);
-        DrawText("-SPEEDY     \"PINKY\"", 155, 200, 30, CL_FROSA);
-        DrawText("-BASHFUL    \"INKY\"", 155, 280, 30, CL_FAZUL);
-        DrawText("-POKEY       \"CLYDE\"", 155, 360, 30, CL_FNARANJA);
-        DrawCircle(250, 500, 3, WHITE);
-        DrawText("10", 270, 485, 30, WHITE);
-        DrawText("PTS", 315, 490, 20, WHITE);
-        DrawCircle(250, 560, 10, WHITE);
-        DrawText("50", 270, 545, 30, WHITE);
-        DrawText("PTS", 315, 550, 20, WHITE);
-        DrawText("PRESS ENTER TO START", 100, 660, 30, CL_FNARANJA);
+        if(menuactivo){
+            menu();
+        } else {
+            ClearBackground(BLACK);
+                DrawText("HIGH SCORE", 204, 0, 30, WHITE);
+
+            DrawLineBezier(Vector2{100, 100}, Vector2{500, 500}, 5, WHITE); 
+        }
         EndDrawing();
     }
     CloseWindow();
     return 0;
+}
+
+void menu(){
+    DrawText("HIGH SCORE", 204, 0, 30, WHITE);
+    DrawText("CHARACTER / NICKNAME", 150, 70, 30, WHITE);
+    DrawText("-SHADOW     \"BLINKY\"", 155, 120, 30, CL_FROJO);
+    DrawText("-SPEEDY     \"PINKY\"", 155, 200, 30, CL_FROSA);
+    DrawText("-BASHFUL    \"INKY\"", 155, 280, 30, CL_FAZUL);
+    DrawText("-POKEY       \"CLYDE\"", 155, 360, 30, CL_FNARANJA);
+    DrawCircle(250, 500, 3, WHITE);
+    DrawText("10", 270, 485, 30, WHITE);
+    DrawText("PTS", 315, 490, 20, WHITE);
+    DrawCircle(250, 560, 10, WHITE);
+    DrawText("50", 270, 545, 30, WHITE);
+    DrawText("PTS", 315, 550, 20, WHITE);
+    DrawText("PRESS ENTER TO START", 100, 660, 30, CL_FNARANJA);
 }
