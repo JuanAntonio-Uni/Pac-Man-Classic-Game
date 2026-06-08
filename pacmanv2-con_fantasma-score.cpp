509775c63a87c8769193c43fdd82beb816b967b8
JuanAntonio-Uni <jluis.torres@hotmail.com>
2026-06-08 15:36:12 -0600

    Add files via upload
    Se completo la implementación de la creación del archivo de texto donde se guardan las puntaciones y se creo la función para mostrarlas, se implemento un ciclo para que terminando el juego no cierre el programa si no que regrese al menú después de un system("pause")  y se hicieron unos pequeños ajustes en el menú.

---
+++ pacmanv2-con_fantasma-score.cpp
@@ -0,0 +1,252 @@
+//Juego Pacman proyecto programacion 
+//Luis Angel Maldonado Valenzuela
+//Juan Antonio Torres Montoya 
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<conio.h>
+#include<math.h>
+
+#define REN 22 //eje y
+#define COL 36 //eje x
+
+struct Pacman {
+    int x;
+    int y;
+};
+
+struct Fantasma{
+    int x;
+    int y;
+};
+
+void menu();
+void cuerpodeljuego(FILE*);
+void enemigo(struct Fantasma *fantasma, struct Pacman *pacman);
+void dibujarmapa(struct Pacman *pacman, struct Fantasma *fantasma);
+void tabladescore(int score, FILE*);
+void leersc(FILE*);
+
+
+/*mapa en el que se movera pacman*/
+char mapa  [REN][COL] = {
+    "###################################",
+    "#..............######.............#",
+    "#0######.#####.######.#####.#####0#",
+    "#.######.#####.######.#####.#####.#",
+    "#.................................#",
+    "#.######.## ###########.##.######.#",
+    "#.######.##.###########.##.######.#",
+    "#........##.....###.....##........#",
+    "########.######.###.######.########",
+    "######## ##.............##.########",
+    "#........##.###########.##........#",
+    "#.######....###########....######.#",
+    "#.######.##.....###.....##.######.#",
+    "#.######.###### ### ######.######.#",
+    "#.######...................######.#",
+    "#.###### ################# ######.#",
+    "#0..............###..............0#",
+    "########.##.###.### ###.## ########",
+    "#....... ##.....###.....##........#",
+    "#.#############.###.#############.#",
+    "#.................................#",
+    "###################################",
+};
+
+int main(){
+    menu();
+}
+
+//Menu de opciones, jugar o salir del juego
+void menu(){
+    int opcion;
+	FILE *arch;
+    while(opcion != 3){
+        system("cls");
+        printf("==============================\n");
+        printf("      Pacman The Game         \n");
+        printf("==============================\n");
+        printf("\n");
+        printf("       [ 1. Jugar   ]         \n");
+        printf("       [ 2. Puntajes]		  \n");
+        printf("       [ 3. Salir   ]         \n");
+        printf("\n");
+        printf("==============================\n");
+        printf("\n");
+        printf("\n");
+        printf("==============================\n");
+        printf("     Ingrese su opcion:     \n");
+        printf("==============================\n");
+        scanf("%d", &opcion);
+        switch(opcion){
+            case 1:
+                cuerpodeljuego(arch); break;
+            case 2:
+                leersc(arch); break;
+            case 3:
+                exit(0); break;
+            default:
+                printf("Opcion no valida\n"); break;
+        }
+    }
+}
+
+
+void cuerpodeljuego(FILE *arch){
+    int juego = 1, j = 1;
+    
+    //inicializacion estructuras para los personajes del juego(blinky y pacman)
+    struct Pacman *pacman = (struct Pacman*)malloc(sizeof(struct Pacman));
+    struct Fantasma *fantasma = (struct Fantasma*)malloc(sizeof(struct Fantasma));
+    
+    //posicion pocman
+    pacman->x = 1;
+    pacman->y = 1;
+    int score = 0;
+    
+    //posicion blinky
+    fantasma->x = 33;
+	fantasma->y = 20;
+	 
+    char pared = '#';
+    char punto = '.';
+    char pastillapoder = '0';
+    
+    while(juego!=0){
+        system("cls");
+        dibujarmapa(pacman,fantasma);
+        enemigo(fantasma,pacman);
+        
+        //Controles del personaje pacman
+        if (_kbhit()) {
+            char tecla = _getch();
+			int sigX = pacman->x;
+			int sigY = pacman->y;
+            if (tecla == 'w' || tecla == 'W') sigY--;
+            if (tecla == 's' || tecla == 'S') sigY++;
+            if (tecla == 'a' || tecla == 'A') sigX--;
+            if (tecla == 'd' || tecla == 'D') sigX++;
+            
+            // Evitar que pacman se mueva por una pared y se haga trampas
+            if (mapa[sigY][sigX] != pared){
+        	pacman->y = sigY;
+        	pacman->x = sigX;
+		
+	        }
+        }
+        
+        char posActual = mapa[pacman->y][pacman->x];
+        
+        if(posActual == punto){
+            score += 10;
+            mapa[pacman->y][pacman->x] = ' ';
+        }
+        if(posActual == pastillapoder){
+            score += 50;
+            mapa[pacman->y][pacman->x] = ' ';
+        }
+        
+        if(pacman->x == fantasma->x && pacman->y == fantasma->y){
+            juego = 0;
+        }
+		
+        _sleep(200);
+    }//while
+
+    system("cls");
+    printf("\n");
+    printf("=========================\n");
+    printf("       Game Over         \n");
+    printf("           :(            \n");
+    printf("=========================\n");
+    printf("      Your score: %d\n\n",score);
+    system("pause");
+    
+    free(pacman);
+    tabladescore(score, arch);
+}
+
+
+
+void dibujarmapa(struct Pacman *pacman, struct Fantasma *fantasma){
+    for(int i = 0; i < REN; i++) {
+        for(int j = 0; j < COL - 1; j++) {
+            if (j == pacman->x && i == pacman->y) {
+                printf("C"); // Dibuja a Pacman 
+			}else if(j == fantasma->x && i == fantasma->y){
+            	printf("A");//Dibuja a Blinky(o un fantasma xd)
+            }else{
+                printf("%c", mapa[i][j]);
+			}
+            
+        }
+        printf("\n");
+    }
+}  
+
+void enemigo(struct Fantasma *fantasma, struct Pacman *pacman){
+	
+	int distancia;
+	float mejdist = 500;
+	int dire = -1;
+	int nextY, nextX;
+	
+	int moveY[] = {-1,0,1,0};
+	int moveX[] = {0,1,0,-1};
+	
+	for(int i=0; i<4; i++){
+		nextY = fantasma->y + moveY[i];
+		nextX = fantasma->x + moveX[i];
+		
+		if (mapa[nextY][nextX] != '#'){
+			distancia = sqrt(pow((pacman->x - nextX), 2) + pow((pacman->y - nextY), 2));
+			
+			if (distancia < mejdist){
+				mejdist = distancia;
+				dire = i;
+			}
+		}
+	}
+	
+	fantasma->x += moveX[dire];
+	fantasma->y += moveY[dire];
+}
+
+void tabladescore(int score, FILE *arch){
+	arch  = fopen("JGscore.txt","a");
+	
+	if(arch == NULL){
+		printf("¡ERROR DE ARCHIVO!");
+	}
+	
+
+    //Guarda el puntuaje de la partida acabada en el archivo de texto
+	fprintf(arch,"%d \n",score);
+	fclose(arch);
+
+}
+
+void leersc(FILE *arch){
+    char punts;
+
+    arch = fopen("JGscore.txt","r");
+
+    printf("\n");
+    printf("Pacman Score:\n");
+
+    //Imprime el contenido del archivo de texto
+    if(arch != NULL){
+        while(!feof(arch)){
+            punts = fgetc(arch);
+            printf("%c", punts);
+        }
+    } else {
+        printf("Error al abrir el archivo");
+    }
+
+    printf("\n");
+    system("pause");
+}
+
